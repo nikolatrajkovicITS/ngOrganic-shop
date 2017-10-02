d78c1901853d92494cdd62b4518960c1c2abdf41
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-10-02 14:53:26 +0200

    Create a bunch of components and routing for them

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDy1AgPHt3xF8f04WlrerA_kN6h3Ur0tug",
+    authDomain: "oshop-84590.firebaseapp.com",
+    databaseURL: "https://oshop-84590.firebaseio.com",
+    projectId: "oshop-84590",
+    storageBucket: "",
+    messagingSenderId: "131750958103"
+  }
+};
