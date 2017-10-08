0f783b03fcc42ef41e96bdf2eedd696204c68dd8
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-10-08 11:24:39 +0200

    Add Displaying categories and filter products by catagory

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
+import { CategoryService } from '../category.service';
 import { ProductService } from '../product.service';
 import { Component } from '@angular/core';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
@@ -7,10 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  categories$;
+  category: string;
 
-  constructor(productService: ProductService) {
-    this.products$ = productService.getAll();
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService, 
+    categoryService: CategoryService) {
+    productService.getAll().subscribe(products => this.products = products);
+    this.categories$ = categoryService.getAll();
    }
+   
+   route.queryParamMap.subscribe(params => {
+     this.category = params.get('category');
 
+     this.filteredProducts = (this.categories$) ?          // seting the filtered products array
+       this.products.filter(p => p.category === this.category) : 
+       this.products;
+   });
 }
