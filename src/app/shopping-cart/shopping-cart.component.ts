16c7edf5750ba0a614f55939eeb0c96962325d56
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-10-10 22:24:40 +0200

    Building the ShoppingCart page, fixed desing issue and Displaying the TotalPrice of shopping cart.

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent implements OnInit {
+  cart$;
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.cart$ = await this.shoppingCartService.getCart();
   }
 
 }
