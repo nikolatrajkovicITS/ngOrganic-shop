67733552818d02da2715c4a7fba78471903cb180
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-10-13 16:57:08 +0200

    Refactoring Order-model, redirecting user on the success oreder page, Associating the Order with the Current user and clearing ShoppingCart

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,8 +1,11 @@
+import { Router } from '@angular/router';
 import { OrderService } from '../order.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ShoppingCartService } from '../shopping-cart.service';
 import { ShoppingCart } from '../models/shopping-cart';
 import { Subscription } from 'rxjs/Subscription';
+import { AuthService } from '../auth.service';
+import { Order } from '../models/oreder';
 
 @Component({
   selector: 'app-check-out',
@@ -12,38 +15,30 @@ import { Subscription } from 'rxjs/Subscription';
 export class CheckOutComponent implements OnInit, OnDestroy {
   shipping = {};
   cart: ShoppingCart;
-  subscription: Subscription;
+  userId: string;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
 
   constructor(
+    private router: Router,
+    private authService: AuthService,
     private orderService: OrderService,
     private shoppingCartService: ShoppingCartService) { }
   
   async ngOnInit() {
     let cart$ = await this.shoppingCartService.getCart();
-    this.subscription = cart$.subscribe(cart => this.cart = cart);
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
   
-  placeOrder() {
-    let order = {
-      datePlaced: new Date().getTime(),
-      shipping: this.shipping,
-      items: this.cart.items.map(i => {
-        return {
-          product: {
-            title: i.title,
-            imageUrl: i.imageUrl,
-            price: i.price
-          },
-          quantity: i.quantity,
-          totalPrice: i.totalPrice
-        }
-      })
-    };
-
-    this.orderService.storeOrder(order);
+  async placeOrder() {
+    let order = new Order(this.userId, this.shipping, this.cart);
+    let result = await this.orderService.storeOrder(order);
+    this.router.navigate(['order-success', result.key]);      // key - we use when we store something in firebase, and $key -when we read a note from Firebase
   }  
 }
