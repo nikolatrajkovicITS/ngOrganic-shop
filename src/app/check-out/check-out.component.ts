bbb2d51370f64bbc8bf22caf4120c9a7174c2033
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-10-14 12:19:42 +0200

    Refactoring ShippingFormComp and added an Order Summary widget.

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,44 +1,20 @@
-import { Router } from '@angular/router';
-import { OrderService } from '../order.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from '../shopping-cart.service';
 import { ShoppingCart } from '../models/shopping-cart';
-import { Subscription } from 'rxjs/Subscription';
-import { AuthService } from '../auth.service';
-import { Order } from '../models/oreder';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit, OnDestroy {
-  shipping = {};
-  cart: ShoppingCart;
-  userId: string;
+export class CheckOutComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
   cartSubscription: Subscription;
-  userSubscription: Subscription;
-
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-    private orderService: OrderService,
-    private shoppingCartService: ShoppingCartService) { }
+  
+  constructor(private shoppingCartService: ShoppingCartService) { }
   
   async ngOnInit() {
-    let cart$ = await this.shoppingCartService.getCart();
-    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
-    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
+    this.cart$ = await this.shoppingCartService.getCart();
   }
-
-  ngOnDestroy() {
-    this.cartSubscription.unsubscribe();
-    this.userSubscription.unsubscribe();
-  }
-  
-  async placeOrder() {
-    let order = new Order(this.userId, this.shipping, this.cart);
-    let result = await this.orderService.storeOrder(order);
-    this.router.navigate(['order-success', result.key]);      // key - we use when we store something in firebase, and $key -when we read a note from Firebase
-  }  
 }
